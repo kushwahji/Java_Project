c6f4d22519e19539621113cb5da6b4bcc24474dc
SANTOSH <santoshkumar021990@gmail.com>
2021-07-07 15:47:57 +0530

    resolve vulnurbility

--- csv_transformation/src/main/java/com/santosh/mail/Mail.java
+++ csv_transformation/src/main/java/com/santosh/mail/Mail.java
@@ -6,14 +6,14 @@ import java.util.Date;
 import java.util.Properties;
 
 public class Mail {
-    String mailFrom = "kushwah.developer@gmail.com";
+    String mailFrom = "xyz@gmail.com";
     public static void sendEmailWithAttachments(String toAddress,
             String subject, String message, String attachFiles)
             throws MessagingException {
         String host = "smtp.gmail.com";
         String port = "587";
-        String mailFrom = "kushwah.developer@gmail.com";
-        String password = "Vihaan@24012018";
+        String mailFrom = "xyz@gmail.com";
+        String password = "***********";
         // sets SMTP server properties
         Properties properties = new Properties();
         properties.put("mail.smtp.host", host);
